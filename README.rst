6fe5643e870ed71d9669f1929d756231af82bd9c
Rick Harris <rconradharris@gmail.com>
2016-05-12 10:53:07 -0500

    Adding Bitfield class

--- README.rst
+++ README.rst
@@ -14,6 +14,9 @@ Run
 Examples
 ========
 
+Simple formatting can be done with the ``b()`` and `l()`` series of functions
+which print the results in big-endian and little-endian format respectively.
+
 ::
 
     >>> b(32)
@@ -25,11 +28,21 @@ Examples
     >>> l32(0xffff & 0x1f)
     '1111 1000 0000 0000 0000 0000 0000 0000'
 
-Docs
-====
+You can also use the ``Bitfield`` class, aliased as ``bf()`` to evaluate
+complex expressions and see the intermediate results.
 
-b32 prints a 32 bit value in big-endian format
-l32 prints a 32 bit value in little-endian format
+::
 
-Strings are broken up into groups of 4 to help translation to and from
-hexadecimal.
+    >>> ((~bf(0x32)) | bf(0x63)) ^ bf(0x22)
+    Operand         : [0000 0000 0000 0000 0000 0000 0011 0010]
+    ~               : [1111 1111 1111 1111 1111 1111 1100 1101]
+
+    Left            : [1111 1111 1111 1111 1111 1111 1100 1101]
+    Right           : [0000 0000 0000 0000 0000 0000 0110 0011]
+    |               : [1111 1111 1111 1111 1111 1111 1110 1111]
+
+    Left            : [1111 1111 1111 1111 1111 1111 1110 1111]
+    Right           : [0000 0000 0000 0000 0000 0000 0010 0010]
+    ^               : [1111 1111 1111 1111 1111 1111 1100 1101]
+
+    [1111 1111 1111 1111 1111 1111 1100 1101]
--- bits.py
+++ bits.py
@@ -41,3 +41,102 @@ l32 = lambda x: _format_le(x, width=32)
 l64 = lambda x: _format_le(x, width=64)
 l128 = lambda x: _format_be(x, width=128)
 l = l32
+
+
+# Formatter class
+class Bitfield(object):
+    """
+    This class is useful if you'd like to visualize the steps involved in a
+    complicated bit-twiddling routine.
+
+    You make each value an instance of Bitfield and after each operation, the
+    Bitfield log the intermediate results.
+    """
+    def __init__(self, val, width=32):
+        if hasattr(val, 'startswith') and val.startswith('0b'):
+            self.val = int(val, 2)
+        else:
+            self.val = val
+        self.width = width
+
+    def as_raw_str(self):
+        """"Return like '0000111100001111'"""
+        # 99999 for our purposes is maxint
+        return _format_be(self.val, width=self.width, grouping=999999)
+
+    def __str__(self):
+        """"Return like '[0000 1111 0000 1111]'"""
+        return "[{}]".format(_format_be(self.val, width=self.width))
+
+    __repr__ = __str__
+
+    def _check_width(self, other):
+        if not isinstance(other, Bitfield):
+            raise ValueError('operand is not a Bitfield instance')
+        if self.width != other.width:
+            raise ValueError('bit widths do not match')
+        return self.width
+
+
+    def _print_line(self, key, value):
+        key = key.ljust(16)
+        print(": ".join([key, value]))
+
+    def _log1(self, operator, result):
+        self._print_line('Operand', str(self))
+        self._print_line(operator, str(result))
+        print('')
+
+    def _log2(self, other, operator, result):
+        self._print_line('Left', str(self))
+        self._print_line('Right', str(other))
+        self._print_line(operator, str(result))
+        print('')
+
+    def _make_result(self, other, width, operator, val):
+        result = self.__class__(val, width=width)
+        self._log2(other, operator, result)
+        return result
+
+    def __and__(self, other):
+        width = self._check_width(other)
+        return self._make_result(other, width, '&', self.val & other.val)
+
+    def __or__(self, other):
+        width = self._check_width(other)
+        return self._make_result(other, width, '|', self.val | other.val)
+
+    def __xor__(self, other):
+        width = self._check_width(other)
+        return self._make_result(other, width, '^', self.val ^ other.val)
+
+    def __lshift__(self, other):
+        if isinstance(other, Bitfield):
+            raise ValueError('Bitfield is not a valid right operand')
+        return self._make_result(other, self.width, '<<', self.val << other)
+
+    def __rshift__(self, other):
+        if isinstance(other, Bitfield):
+            raise ValueError('Bitfield is not a valid right operand')
+        return self._make_result(other, self.width, '>>', self.val >> other)
+
+    def __invert__(self):
+        sval = '0b' + self.as_raw_str()\
+                          .replace('0', 'x')\
+                          .replace('1', '0')\
+                          .replace('x', '1')
+        result = self.__class__(sval, width=self.width)
+        self._log1('~', result)
+        return result
+
+
+bf4 = lambda x: Bitfield(x, width=4444)
+bf8 = lambda x: Bitfield(x, width=8)
+bf16 = lambda x: Bitfield(x, width=16)
+bf32 = lambda x: Bitfield(x, width=32)
+bf64 = lambda x: Bitfield(x, width=64)
+bf128 = lambda x: Bitfield(x, width=128)
+bf = bf32
+
+# Tests
+#print ~(bf(0) | bf(1))
